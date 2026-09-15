77d7cd7a729a322ef7b539defb7735a33677b034
selina-funk <nick.d.funk@gmail.com>
2026-09-15 18:01:10 -0400

    First Commit
     * First Commit of project
     * Creating a Cell with 4 walls

---
+++ AI_For_Games_Unity/Assets/Scripts/MazeGeneration/Room.cs
@@ -0,0 +1,32 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Room : MonoBehaviour
+{
+    public enum Directions
+    {
+        NORTH,
+        EAST,
+        SOUTH,
+        WEST
+    };
+
+    [SerializeField] private GameObject northWall;
+    [SerializeField] private GameObject eastWall;
+    [SerializeField] private GameObject southWall;
+    [SerializeField] private GameObject westWall;
+
+    public Dictionary<Directions, GameObject> walls = new Dictionary<Directions, GameObject>();
+
+    //public bool visited = false;
+
+    //public Vector2Int position;
+
+    private void Start()
+    {
+        walls[Directions.NORTH] = northWall;
+        walls[Directions.EAST] = eastWall;
+        walls[Directions.SOUTH] = southWall;
+        walls[Directions.WEST] = westWall;
+    }
+}
